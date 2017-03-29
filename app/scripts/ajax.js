52d311945c442cf3a5fcaecaafb3111a2d30056a
Anthony Nguyen <anthonypn93@gmail.com>
2017-03-28 20:00:03 -0400

    Evolution works

--- app/scripts/ajax.js
+++ app/scripts/ajax.js
@@ -1,24 +1,57 @@
 $(document).ready(function(){
+	var pokeId = 0;
+
 	function GetPokemon(pokemon){
 		$.getJSON('http://pokeapi.co/api/v2/pokemon/' + pokemon + '/', function(data){
 			console.log("Name: " + data.name);
+			console.log("Id: " + data.id);
+			pokeId = data.id;
+
 			$("#name").empty();
+			$("#num").empty();
 			$("#type").empty();
 			$("#move").empty();
+
 			$("#name").html(data.name);
+			$("#number").html(data.id);
 
 			$.each(data.types, function(i, item){
 				console.log("Type " + (i+1) + ": " + data.types[i].type.name);
-				/*var types = {};
-				var types[i] = data.types[i].type.name;
-				$("#type").html(types);*/
-				$("#type").append(data.types[i].type.name);
+				$("#type").append(data.types[i].type.name + "<br>");
 			});
 
 			$.each(data.moves, function(i, item){
 				if(data.moves[i].version_group_details[0].move_learn_method.name == "level-up"){
 					console.log("Move " + (i+1) + ": " + data.moves[i].move.name);
-					$("#move").append(data.moves[i].move.name);
+					$("#move").append(data.moves[i].move.name + "<br>");
+				}
+			});
+		});
+	}
+
+	function Evolve(pokemon){
+		$.getJSON('http://pokeapi.co/api/v2/pokemon/' + pokemon + '/', function(data){
+			console.log("Name: " + data.name);
+			console.log("Id: " + data.id);
+			pokeId = data.id;
+
+			$("#name").empty();
+			$("#num").empty();
+			$("#type").empty();
+			$("#move").empty();
+
+			$("#name").html(data.name);
+			$("#number").html(data.id);
+
+			$.each(data.types, function(i, item){
+				console.log("Type " + (i+1) + ": " + data.types[i].type.name);
+				$("#type").append(data.types[i].type.name + "<br>");
+			});
+
+			$.each(data.moves, function(i, item){
+				if(data.moves[i].version_group_details[0].move_learn_method.name == "level-up"){
+					console.log("Move " + (i+1) + ": " + data.moves[i].move.name);
+					$("#move").append(data.moves[i].move.name + "<br>");
 				}
 			});
 		});
@@ -29,5 +62,9 @@ $(document).ready(function(){
 		var pokemon = $(this).serialize();
 		//console.log(pokemon.substr(6));
 		GetPokemon(pokemon.substr(6));
+	});
+
+	$("#evolve").click(function(){
+		Evolve(pokeId + 1);
 	});
 });
